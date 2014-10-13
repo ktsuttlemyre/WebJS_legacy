2580d18a79bb187536b61babbe928346c4c59bba
ktsuttlemyre <ktsuttlemyre@gmail.com>
2014-10-12 22:23:14 -0400

    debugging server and using git as a means to keep workflow easy

--- web.js
+++ web.js
@@ -51,11 +51,13 @@ var web=(function(web,global,environmentFlags,undefined){
 	};
 
 	web.isNodeJS=environmentFlags.platform=='nodejs';
+	web.env = (web.isNodeJS)?process.env.NODE_ENV : 'development' //TODO allow setting of this for webpages. maybe use a tag or something? maybe hashFragment? idk
+
 	web.global = global;
 	global.web=web
 	web.environment=environmentFlags;
-	var _=(typeof _!='undefined')?_:require('lodash')
-	var $=(typeof $!='undefined')?$:require('cheerio')
+	var _=(web.global._)?web.global._:require('lodash')
+	var $=(web.global.$)?web.global.$:require('cheerio')
 
 
 web.extend=function(a1,a2){
@@ -941,31 +943,32 @@ web.warn=_.bind(console.warn,console);
 //http://trac.webkit.org/changeset/130941
 //http://jsfiddle.net/yg6hk/5/
 function styledConsoleLog() {
-    var argArray = [];
+	if(!web.isNodeJS){
+	    var argArray = [];
 
-    if (arguments.length) {
-        var startTagRe = /<span\s+style=(['"])([^'"]*)\1\s*>/gi;
-        var endTagRe = /<\/span>/gi;
+	    if (arguments.length) {
+	        var startTagRe = /<span\s+style=(['"])([^'"]*)\1\s*>/gi;
+	        var endTagRe = /<\/span>/gi;
 
-        var reResultArray;
-        argArray.push(arguments[0].replace(startTagRe, '%c').replace(endTagRe, '%c'));
-        while (reResultArray = startTagRe.exec(arguments[0])) {
-            argArray.push(reResultArray[2]);
-            argArray.push('');
-        }
+	        var reResultArray;
+	        argArray.push(arguments[0].replace(startTagRe, '%c').replace(endTagRe, '%c'));
+	        while (reResultArray = startTagRe.exec(arguments[0])) {
+	            argArray.push(reResultArray[2]);
+	            argArray.push('');
+	        }
 
-        // pass through subsequent args since chrome dev tools does not (yet) support console.log styling of the following form: console.log('%cBlue!', 'color: blue;', '%cRed!', 'color: red;');
-        for (var j = 1; j < arguments.length; j++) {
-            argArray.push(arguments[j]);
-        }
-    }
-
+	        // pass through subsequent args since chrome dev tools does not (yet) support console.log styling of the following form: console.log('%cBlue!', 'color: blue;', '%cRed!', 'color: red;');
+	        for (var j = 1; j < arguments.length; j++) {
+	            argArray.push(arguments[j]);
+	        }
+	    }
+	}
     console.log.apply(console, argArray);
 }
 
 styledConsoleLog('<span style="color:hsl(0, 100%, 90%);background-color:hsl(0, 100%, 50%);"> Red </span> <span style="color:hsl(39, 100%, 85%);background-color:hsl(39, 100%, 50%);"> Orange </span> <span style="color:hsl(60, 100%, 35%);background-color:hsl(60, 100%, 50%);"> Yellow </span> <span style="color:hsl(120, 100%, 60%);background-color:hsl(120, 100%, 25%);"> Green </span> <span style="color:hsl(240, 100%, 90%);background-color:hsl(240, 100%, 50%);"> Blue </span> <span style="color:hsl(300, 100%, 85%);background-color:hsl(300, 100%, 25%);"> Purple </span> <span style="color:hsl(0, 0%, 80%);background-color:hsl(0, 0%, 0%);"> Black </span>');
 
-function styledMODIFIEDConsoleLog() { //MODIFIEDDD
+function styledMODIFIEDConsoleLog() { //MODIFIED
     var argArray = [];
 
     if (arguments.length) {
